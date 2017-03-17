0181aedad9daa6a6fb45d2a7bc663f719ea1d678
jesse <jsoldat@hotmail.com>
2017-03-17 09:56:24 +0700

    can view details

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
+	
+
+
 	axios.get('http://www.omdbapi.com?s='+searchText)
 		.then((res) => {
 			// console.log(res);
@@ -38,8 +41,48 @@ function movieSelected(id) {
 	return false;
 }
 
+
 function getMovie() {
 	let movieId = sessionStorage.getItem('movieId')
-	console.log(movieId);
+	
+	axios.get('http://www.omdbapi.com?i='+movieId)
+		.then((res) => {
+			let movie = res.data;
+			let output = `
+			<div class='row'>
+				<div class='col-md-4'>
+					<img src='${movie.Poster}' class='thumbnail'>
+				</div>
+				<div class='col-md-8'>
+					<h2>${movie.Title}</h2>
+					<ul class='list-group'>
+						<li class='list-group-item'><strong>Genre: </strong>${movie.Genre}</li>
+						<li class='list-group-item'><strong>Released: </strong>${movie.Released}</li>
+						<li class='list-group-item'><strong>Rated: </strong>${movie.Rated}</li>
+						<li class='list-group-item'><strong>Director: </strong>${movie.Director}</li>
+						<li class='list-group-item'><strong>Writer: </strong>${movie.Writer}</li>
+						<li class='list-group-item'><strong>Actors: </strong>${movie.Actors}</li>
+					<ul>
+				</div>
+				<div class='row'>
+					<div class='well'>
+					<h3>Plot</h3>
+					${movie.Plot}
+					<hr>
+					<a href='http://imdb.com/title/${movie.imdbID}' target='_blank' class='btn btn-primary'>
+						View IMDB
+					</a>
+					<a href='index.html' class='btn btn-default'>
+						 Go Back to Search
+					</a>
+					
+				</div>
+			</div>
+			`
+			$('#movie').html(output);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 
 }
